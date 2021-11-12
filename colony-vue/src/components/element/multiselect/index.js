876c0f80771f20d9d3ef4d2a5405d3636be3f636
es5es5 <es5e5s@nate.com>
2021-11-12 10:47:51 +0900

    Removed submodules

--- colony-vue/src/components/element/multiselect/index.js
+++
@@ -1,7 +0,0 @@
-import MultiSelect from './MultiSelect'
-import multiselectMixin from './multiselectMixin'
-import pointerMixin from './pointerMixin'
-
-export default MultiSelect
-
-export { MultiSelect, multiselectMixin, pointerMixin }
