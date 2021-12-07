a26901aadb74b1b248e00f1360886f097f7533ba
es5es5 <es5e5s@nate.com>
2021-12-07 14:29:24 +0900

    updater logger

--- src/background.js
+++ src/background.js
@@ -5,11 +5,30 @@ import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 import { autoUpdater } from 'electron-updater'
+import log from 'electron-log'
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
   { scheme: 'app', privileges: { secure: true, standard: true } }
 ])
+
+autoUpdater.on('checking-for-update', () => log.info('업데이트 확인 중...'))
+autoUpdater.on('update-available', info => {
+  log.info('업데이트가 가능합니다.')
+  log.log(info)
+})
+autoUpdater.on('update-not-available', () => log.info('현재 최신버전입니다.'))
+autoUpdater.on('error', err => log.info('에러가 발생하였습니다. 에러내용 : ' + err))
+
+autoUpdater.on('download-progress', progressObj => {
+  let log_message = "다운로드 속도: " + progressObj.bytesPerSecond
+  log_message = log_message + ' - 현재 ' + progressObj.percent + '%'
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')'
+  log.info(log_message)
+})
+autoUpdater.on('update-downloaded', (info) => {
+  log.info('업데이트가 완료되었습니다.')
+})
 
 async function createWindow () {
   // Create the browser window.
