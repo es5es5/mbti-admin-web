af0efa84430a42a127d85b3f23f7ba297d053ab1
es5es5 <es5e5s@nate.com>
2021-12-14 10:30:37 +0900

    updateWin

--- src/background.js
+++ src/background.js
@@ -1,10 +1,11 @@
 'use strict'
 
-import { app, protocol, BrowserWindow } from 'electron'
+import { app, protocol, BrowserWindow, dialog } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 import { autoUpdater } from 'electron-updater'
+import { version } from '../package.json'
 import log from 'electron-log'
 
 // Scheme must be registered before the app is ready
@@ -12,33 +13,48 @@ protocol.registerSchemesAsPrivileged([
   { scheme: 'app', privileges: { secure: true, standard: true } }
 ])
 
-let win;
+let updateWin;
+let originWin;
 
 function sendStatusToWindow(text) {
   log.info(text);
-  win.webContents.send('message', text);
+  updateWin.webContents.send('message', text);
 }
 function createUpdateWindow() {
-  win = new BrowserWindow({
+  updateWin = new BrowserWindow({
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
     }
   });
-  win.webContents.openDevTools();
-  win.on('closed', () => {
-    win = null;
-  });
-  win.loadURL(`file://${__dirname}/version.html#v${app.getVersion()}`);
-  return win;
+  updateWin.on('closed', () => {
+    updateWin = null;
+  })
+  updateWin.loadURL(`file://${__dirname}/version.html#v${app.getVersion()}`)
+  return updateWin
 }
 
-autoUpdater.on('checking-for-update', () => log.info('업데이트 확인 중...'))
-autoUpdater.on('update-available', info => {
-  log.info('업데이트가 가능합니다.')
-  log.log(info)
+autoUpdater.on('checking-for-update', () => {
+  createUpdateWindow()
+  log.info('업데이트 확인 중...')
 })
-autoUpdater.on('update-not-available', () => log.info('현재 최신버전입니다.'))
+autoUpdater.on('update-available', info => {
+  log.info(`Current Version: ${version}`)
+  log.info('업데이트가 가능합니다.')
+  log.info(info)
+
+  sendStatusToWindow(`Current Version: ${version}`)
+  sendStatusToWindow('업데이트가 가능합니다.')
+  sendStatusToWindow(info)
+})
+autoUpdater.on('update-not-available', () => {
+  log.info(`Current Version: ${version}`)
+  log.info('현재 최신버전입니다.')
+
+  sendStatusToWindow(`Current Version: ${version}`)
+  sendStatusToWindow('현재 최신버전입니다.')
+})
+
 autoUpdater.on('error', err => log.info('에러가 발생하였습니다. 에러내용 : ' + err))
 
 autoUpdater.on('download-progress', progressObj => {
@@ -46,11 +62,11 @@ autoUpdater.on('download-progress', progressObj => {
   log_message = log_message + ' - 현재 ' + progressObj.percent + '%'
   log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')'
   log.info(log_message)
+
+  sendStatusToWindow(log_message)
 })
 autoUpdater.on('update-downloaded', (info) => {
-  createUpdateWindow()
-  sendStatusToWindow("Update downloaded");
-
+  sendStatusToWindow("Update downloaded")
   const option = {
     type: "question",
     buttons: ["업데이트", "취소"],
@@ -58,7 +74,7 @@ autoUpdater.on('update-downloaded', (info) => {
     title: "electron-updater",
     message: "업데이트가 있습니다. 프로그램을 업데이트 하시겠습니까?",
   };
-  let btnIndex = dialog.showMessageBoxSync(updateWin, option);
+  let btnIndex = dialog.showMessageBoxSync(originWin, option);
 
   if(btnIndex === 0) {
     autoUpdater.quitAndInstall();
@@ -68,7 +84,7 @@ autoUpdater.on('update-downloaded', (info) => {
 
 async function createWindow () {
   // Create the browser window.
-  const win = new BrowserWindow({
+  originWin = new BrowserWindow({
     width: 1440,
     height: 900,
     webPreferences: {
@@ -82,12 +98,12 @@ async function createWindow () {
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
-    await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
+    await originWin.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
+    if (!process.env.IS_TEST) originWin.webContents.openDevTools()
   } else {
     createProtocol('app')
     // Load the index.html when not in development
-    win.loadURL('app://./index.html')
+    originWin.loadURL('app://./index.html')
     autoUpdater.checkForUpdatesAndNotify()
   }
 }
